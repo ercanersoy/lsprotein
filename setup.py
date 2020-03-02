84243928ec5f29af1a15a0e64c96a529381ff196
Ercan Ersoy <ercanersoy@ercanersoy.net>
2020-03-02 05:50:39 +0300

    Add setup script
    Signed-off-by: Ercan Ersoy <ercanersoy@ercanersoy.net>

---
+++ setup.py
@@ -0,0 +1,27 @@
+# /usr/bin/env python3
+
+# setup.py - Setup source file of lsprotein
+# Written by Ercan Ersoy (http://ercanersoy.net).
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lsprotein',
+    description='Protein lister on command line interface',
+    keywords='protein ls lister pdb protein-data-bank protein-database \
+    rest-api command-line cli command-line-interface',
+    version='0.1',
+    author='Ercan Ersoy',
+    author_email='ercanersoy@ercanersoy.net',
+    maintainer='Ercan Ersoy',
+    maintainer_email='ercanersoy@ercanersoy.net',
+    url='https://github.com/ercanersoy/lsprotein',
+    license='MIT',
+    packages=find_packages(),
+    scripts=['lsprotein'],
+    install_requires=['beautifulsoup4'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
